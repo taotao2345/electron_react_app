2897c9a2125ccdfe3ade7b51dc13faa5c448bd0b
takuogaw <takuogaw@yahoo-corp.jp>
2017-05-18 01:07:40 +0900

    add fetch

--- src/renderer/components/Top/Top.jsx
+++ src/renderer/components/Top/Top.jsx
@@ -1,16 +1,45 @@
 import React from "react";
+import fetch from "isomorphic-fetch";
 import { Link } from "react-router";
 import s from "./Top.css";
 
 export default class Top extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      test: null,
+    };
+
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch("https://httpbin.org/ip", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }
+    })
+    .then(res => {
+      if (res.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return res.json();
+    })
+    .then(json => {
+      this.setState({
+        test: json.origin
+      });
+    });
   }
 
   render() {
     return (
       <div className={s.root}>
-        <Link to="/second">to second</Link>
+        <Link to="/second">to second {this.state.test}</Link>
       </div>
     );
   }
